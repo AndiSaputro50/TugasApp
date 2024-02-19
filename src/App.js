0ee53b89e7f81bf1c0157e92462a415f4f536696
AndiSaputro50 <AndiSaputro50>
2024-02-19 09:05:21 +0700

    update

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/navbar';
+import Dashboard from './pages/dashboard';
+import Tentang from './pages/tentang';
+import Siswa from './pages/siswa';
+import Tambah from './crud/tambah';
+import Edit from './crud/edit';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+function App() {
+  return (
+    <>
+      <Navbar />
+      <BrowserRouter>
+        <main>
+          <Switch>
+            <Route path="/" component={Dashboard} exact />
+            <Route path="/tentang" component={Tentang} exact />
+            <Route path="/siswa" component={Siswa} exact />
+            <Route path="/tambah" component={Tambah} exact />
+            <Route path="/edit/:id" component={Edit} exact />
+          </Switch>
+        </main>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
